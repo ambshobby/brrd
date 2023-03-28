d22104eb6859501dafb75fdabd4f7c6543b2768a
ambshobby <70990274+ambshobby@users.noreply.github.com>
2023-03-28 10:20:55 +0530

    Add files via upload

---
+++ R1.java
@@ -0,0 +1,14 @@
+import java.lang.Math;   
+class R1 {
+    public static void main(String args[])   
+{   
+// Generating random numbers 
+ 
+System.out.println("1st Random Number: in the program" + Math.random());
+
+ 
+
+System.out.println("2nd Random Number: in the program" + Math.random());
+}
+}   
+
